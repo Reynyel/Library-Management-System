6fb46e6bec9e69bb83e029b8a9790c79861f321b
Ranielle <raniellecruz65@gmail.com>
2023-07-10 14:25:50 +0800

    add components to log in frame

--- src/LogInFrame.java
+++ src/LogInFrame.java
@@ -3,10 +3,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class LogInFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField usernameTxt;
+	private JPasswordField passwordTxt;
 
 	/**
 	 * Launch the application.
@@ -29,11 +36,39 @@ public class LogInFrame extends JFrame {
 	 */
 	public LogInFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 581, 393);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null); //sets layout to absolute
+		this.setTitle("LogIn"); //Sets title for this frame
+		this.setResizable(false);
 
 		setContentPane(contentPane);
+		
+		JLabel userLabel = new JLabel("Username");
+		userLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		userLabel.setBounds(123, 142, 75, 14);
+		contentPane.add(userLabel);
+		
+		JLabel passLabel = new JLabel("Password");
+		passLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		passLabel.setBounds(123, 176, 75, 14);
+		contentPane.add(passLabel);
+		
+		JButton logBtn = new JButton("Log In");
+		logBtn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		logBtn.setBounds(236, 216, 95, 33);
+		contentPane.add(logBtn);
+		
+		usernameTxt = new JTextField();
+		usernameTxt.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		usernameTxt.setBounds(208, 141, 212, 20);
+		contentPane.add(usernameTxt);
+		usernameTxt.setColumns(10);
+		
+		passwordTxt = new JPasswordField();
+		passwordTxt.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		passwordTxt.setBounds(208, 175, 212, 20);
+		contentPane.add(passwordTxt);
 	}
-
 }
