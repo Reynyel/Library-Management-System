85c745a74363a4c3136b095e551bf7afb0fb2b6a
Ranielle <raniellecruz65@gmail.com>
2023-07-14 13:24:48 +0800

    added placeholder image for book registration button

--- src/MainMenuFrame.java
+++ src/MainMenuFrame.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class MainMenuFrame extends JFrame {
 
@@ -33,17 +35,20 @@ public class MainMenuFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 979, 610);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnRegisterBooks = new JButton("Register Books");
-		btnRegisterBooks.setBounds(193, 139, 148, 108);
+		btnRegisterBooks.setIcon(new ImageIcon("C:\\Users\\LINDELL\\Projects\\Library-Management-System\\book1.jpg"));
+		btnRegisterBooks.setBounds(193, 139, 168, 108);
+		btnRegisterBooks.setBorderPainted(false);
 		contentPane.add(btnRegisterBooks);
 		
 		JButton btnSearchBooks = new JButton("Search Books");
-		btnSearchBooks.setBounds(406, 139, 148, 108);
+		btnSearchBooks.setBounds(551, 139, 148, 108);
 		contentPane.add(btnSearchBooks);
 		
 		JButton btnRegisterUser = new JButton("Register Users");
@@ -53,5 +58,9 @@ public class MainMenuFrame extends JFrame {
 		JButton btnLogOut = new JButton("Log out");
 		btnLogOut.setBounds(10, 527, 93, 33);
 		contentPane.add(btnLogOut);
+		
+		JButton btnReport = new JButton("Report");
+		btnReport.setBounds(551, 312, 148, 108);
+		contentPane.add(btnReport);
 	}
 }
